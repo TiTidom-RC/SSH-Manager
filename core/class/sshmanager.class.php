ce6e8fd1fdb47f882ace32e9e7c1dca2076d0a8e
Mips2648 <mips2648@gmail.com>
2024-09-03 10:36:41 +0200

    add sshmanager.class

---
+++ core/class/sshmanager.class.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__  . '/../../../../core/php/core.inc.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class sshmanager extends eqLogic {
+}
+
+class sshmanagerCmd extends cmd {
+}
