22413de8be677d528220784075551501956ac42a
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-09-03 11:04:09 +0200

    Update sshmanager.class.php

--- core/class/sshmanager.class.php
+++ core/class/sshmanager.class.php
@@ -3,7 +3,51 @@
 require_once __DIR__  . '/../../../../core/php/core.inc.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+use phpseclib3\Net\SSH2;
+use phpseclib3\Crypt\PublicKeyLoader;
+
+if (!defined('NET_SSH2_LOGGING')) {
+	define('NET_SSH2_LOGGING', 2);
+}
+
 class sshmanager extends eqLogic {
+    public function decrypt() {
+		$this->setConfiguration('user', utils::decrypt($this->getConfiguration('user')));
+		$this->setConfiguration('password', utils::decrypt($this->getConfiguration('password')));
+		$this->setConfiguration('ssh-key', utils::decrypt($this->getConfiguration('ssh-key')));
+		$this->setConfiguration('ssh-passphrase', utils::decrypt($this->getConfiguration('ssh-passphrase')));
+	}
+	
+	public function encrypt() {
+		$this->setConfiguration('user', utils::encrypt($this->getConfiguration('user')));
+		$this->setConfiguration('password', utils::encrypt($this->getConfiguration('password')));
+		$this->setConfiguration('ssh-key', utils::encrypt($this->getConfiguration('ssh-key')));
+		$this->setConfiguration('ssh-passphrase', utils::encrypt($this->getConfiguration('ssh-passphrase')));
+	}
+
+    public static function getPluginVersion()
+    {
+        $pluginVersion = '0.0.0';
+		try {
+			if (!file_exists(dirname(__FILE__) . '/../../plugin_info/info.json')) {
+				log::add(__CLASS__, 'warning', '[VERSION] fichier info.json manquant');
+			}
+			$data = json_decode(file_get_contents(dirname(__FILE__) . '/../../plugin_info/info.json'), true);
+			if (!is_array($data)) {
+				log::add(__CLASS__, 'warning', '[VERSION] Impossible de décoder le fichier info.json');
+			}
+			try {
+				$pluginVersion = $data['pluginVersion'];
+			} catch (\Exception $e) {
+				log::add(__CLASS__, 'warning', '[VERSION] Impossible de récupérer la version du plugin');
+			}
+		}
+		catch (\Exception $e) {
+			log::add(__CLASS__, 'warning', '[VERSION] Get ERROR :: ' . $e->getMessage());
+		}
+		log::add('Monitoring', 'info', '[VERSION] PluginVersion :: ' . $pluginVersion);
+        return $pluginVersion;
+    }
 }
 
 class sshmanagerCmd extends cmd {
