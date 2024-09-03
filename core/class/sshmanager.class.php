bbfbcdc634c513a9788a64b25d4b424addf942aa
Mips2648 <mips2648@gmail.com>
2024-09-03 18:04:50 +0200

    add try catch and logs

--- core/class/sshmanager.class.php
+++ core/class/sshmanager.class.php
@@ -11,6 +11,9 @@ if (!defined('NET_SSH2_LOGGING')) {
     define('NET_SSH2_LOGGING', 2);
 }
 
+class SSHConnectException extends \RuntimeException {
+}
+
 class sshmanager extends eqLogic {
 
     const CONFIG_USERNAME = 'username';
@@ -73,7 +76,14 @@ class sshmanager extends eqLogic {
         return $hosts;
     }
 
-    public static function executeCmds(int $hostId, array $commands) {
+    /**
+     * execute ssh cmd on the remote host provided by hostId
+     *
+     * @param int $hostId
+     * @param array $commands
+     * @return array $results
+     */
+    public static function executeCmds($hostId, array $commands) {
         /** @var sshmanager */
         $sshmanager = eqLogic::byId($hostId);
         if (!is_object($sshmanager)) {
@@ -146,7 +156,13 @@ class sshmanager extends eqLogic {
                     log::add(__CLASS__, 'error', 'SSH key not defined');
                     throw new RuntimeException('SSH key not defined');
                 }
-                $keyOrpassword = PublicKeyLoader::load($sshkey, $sshpassphrase);
+                try {
+                    $keyOrpassword = PublicKeyLoader::load($sshkey, $sshpassphrase);
+                } catch (\phpseclib3\Exception\NoKeyLoadedException $ex) {
+                    log::add(__CLASS__, 'error', $ex->getMessage());
+                    throw $ex;
+                }
+
                 break;
             case self::AUTH_METHOD_AGENT:
                 //TODO: check if agent auth could be usefull?
@@ -189,12 +205,28 @@ class sshmanager extends eqLogic {
         [$username, $keyOrpassword] = $this->getAuthenticationData();
 
         $ssh = new SSH2($host, $port, $timeout);
+        try {
+            if (!$ssh->login($username, $keyOrpassword)) {
+                throw new SSHConnectException("[{$this->getName()}] Login failed for {$username}@{$host}:{$port}; please check username and password or ssh key.");
+            }
 
-        if (!$ssh->login($username, $keyOrpassword)) {
-            $error = "Authentification SSH KO";
-            log::add(__CLASS__, 'error', $error, 'authKO');
-            throw new Exception($error);
+            if (!$ssh->isConnected()) {
+                throw new SSHConnectException("[{$this->getName()}] Connexion failed:" . $ssh->getLastError());
+            }
+
+            if (!$ssh->isAuthenticated()) {
+                throw new SSHConnectException("[{$this->getName()}] Authentication failed:" . $ssh->getLastError());
+            }
+        } catch (SSHConnectException $ex) {
+            log::add(__CLASS__, 'error', $ex->getMessage());
+            throw $ex;
+        } catch (\Throwable $th) {
+            log::add(__CLASS__, 'error', "[{$this->getName()}] General exception during connection: " . $th->getMessage() . " - log: " . $ssh->getLog());
+            log::add(__CLASS__, 'error', "[{$this->getName()}] log: " . $ssh->getLog());
+            throw $th;
         }
+
+        log::add(__CLASS__, 'debug', "[{$this->getName()}] Connected and authenticated");
 
         $results = [];
         foreach ($commands as $cmd) {
@@ -205,91 +237,6 @@ class sshmanager extends eqLogic {
         }
 
         return $results;
-    }
-
-    public function execSSH($_commands = array()) {
-        // TODO : Méthode extraite du plugin Monitoring, à adapter pour SSHManager.
-        $equipement = $this->getName();
-        $confLocalOrRemote = $this->getConfiguration('maitreesclave'); // local ou déporté, et si déporté (qui nous intéresse ici), par mot de passe ou par clé
-
-        if (($confLocalOrRemote == 'deporte' || $confLocalOrRemote == 'deporte-key') && $this->getIsEnable()) {
-            $ip = $this->getConfiguration('addressip');
-            $port = $this->getConfiguration('portssh', 22);
-            $timeout = $this->getConfiguration('timeoutssh', 30);
-            $user = $this->getConfiguration('user');
-            $pass = $this->getConfiguration('password');
-            $sshkey = $this->getConfiguration('ssh-key');
-            $sshpassphrase = $this->getConfiguration('ssh-passphrase');
-            $cnx_ssh = '';
-
-
-            // Début de la connexion SSH
-            try {
-                $sshconnection = new SSH2($ip, $port, $timeout);
-                log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH :: IP/Port: ' . $ip . ':' . $port . ' / Timeout: ' . $timeout);
-            } catch (Exception $e) {
-                log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] Connexion SSH :: ' . $e->getMessage());
-                $cnx_ssh = 'KO';
-            }
-
-            if ($cnx_ssh != 'KO') {
-                if ($confLocalOrRemote == 'deporte-key') {
-                    try {
-                        $keyOrPwd = PublicKeyLoader::load($sshkey, $sshpassphrase);
-                        log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] PublicKeyLoader :: OK');
-                    } catch (Exception $e) {
-                        log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] PublicKeyLoader :: ' . $e->getMessage());
-                        $keyOrPwd = '';
-                    }
-                } else {
-                    $keyOrPwd = $pass;
-                    log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Authentification SSH par Mot de passe');
-                }
-
-                try {
-                    if (!$sshconnection->login($user, $keyOrPwd)) {
-                        log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] Login ERROR :: ' . $user);
-                        $cnx_ssh = 'KO';
-                    }
-                } catch (Exception $e) {
-                    log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] Authentification SSH :: ' . $e->getMessage());
-                    $cnx_ssh = 'KO';
-                }
-
-                try {
-                    if ($sshconnection->isConnected()) {
-                        log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH (isConnected) :: OK');
-                        if ($sshconnection->isAuthenticated()) {
-                            log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH (isAuthenticated) :: OK');
-                        } else {
-                            log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] Connexion SSH (isAuthenticated) :: KO');
-                            log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH LastError :: ' . $sshconnection->getLastError());
-                            $cnx_ssh = 'KO';
-                        }
-                    } else {
-                        log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] Connexion SSH (isConnected) :: KO');
-                        log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH LastError :: ' . $sshconnection->getLastError());
-                        $cnx_ssh = 'KO';
-                    }
-                } catch (Exception $e) {
-                    log::add(__CLASS__, 'error', '[' . $equipement . '][connectSSH] Connexion SSH :: ' . $e->getMessage());
-                    log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH Log :: ' . $sshconnection->getLog());
-                    $cnx_ssh = 'KO';
-                }
-
-                // Fin de la connexion SSH
-                if ($cnx_ssh != 'KO') {
-                    $cnx_ssh = 'OK';
-                    log::add(__CLASS__, 'debug', '[' . $equipement . '][connectSSH] Connexion SSH (cnx_ssh) :: OK');
-
-                    $result = array();
-                    foreach ($_commands as $command) {
-                        $result[$command] = $sshconnection->exec($command);
-                    }
-                    // Suite du code à exécuter si la connexion SSH est OK
-                }
-            }
-        }
     }
 
     public function preInsert() {
