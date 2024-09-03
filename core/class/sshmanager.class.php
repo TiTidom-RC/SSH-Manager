974bec83b31643b237fd47509648c8de5781c148
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-09-03 11:14:46 +0200

    Update sshmanager.class.php

--- core/class/sshmanager.class.php
+++ core/class/sshmanager.class.php
@@ -27,8 +27,7 @@ class sshmanager extends eqLogic {
 		$this->setConfiguration('ssh-passphrase', utils::encrypt($this->getConfiguration('ssh-passphrase')));
 	}
 
-    public static function getPluginVersion()
-    {
+    public static function getPluginVersion() {
         $pluginVersion = '0.0.0';
 		try {
 			if (!file_exists(dirname(__FILE__) . '/../../plugin_info/info.json')) {
@@ -49,6 +48,89 @@ class sshmanager extends eqLogic {
 		}
 		log::add(__CLASS__, 'info', '[VERSION] PluginVersion :: ' . $pluginVersion);
         return $pluginVersion;
+    }
+
+    public function execSSH() {
+        // TODO : Méthode extraite du plugin Monitoring, à adapter pour SSHManager.
+        $equipement = $this->getName();
+        $confLocalOrRemote = $this->getConfiguration('maitreesclave'); // local ou déporté, et si déporté, par mot de passe ou par clé
+
+        if (($confLocalOrRemote == 'deporte' || $confLocalOrRemote == 'deporte-key') && $this->getIsEnable()) {
+            $ip = $this->getConfiguration('addressip');
+            $port = $this->getConfiguration('portssh', 22);
+            $timeout = $this->getConfiguration('timeoutssh', 30);
+            $user = $this->getConfiguration('user');
+            $pass = $this->getConfiguration('password');
+            $sshkey = $this->getConfiguration('ssh-key');
+            $sshpassphrase = $this->getConfiguration('ssh-passphrase');
+            $cnx_ssh = '';
+
+
+            // Début de la connexion SSH
+            try {
+                $sshconnection = new SSH2($ip, $port, $timeout);
+                log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH :: IP/Port: ' . $ip . ':' . $port . ' / Timeout: ' . $timeout);
+            } catch (Exception $e) {
+                log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] Connexion SSH :: '. $e->getMessage());
+                $cnx_ssh = 'KO';
+            }
+
+            if ($cnx_ssh != 'KO') {
+                if ($confLocalOrRemote == 'deporte-key') {
+                    try {
+                        $keyOrPwd = PublicKeyLoader::load($sshkey, $sshpassphrase);
+                        log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] PublicKeyLoader :: OK');
+                    } catch (Exception $e) {
+                        log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] PublicKeyLoader :: '. $e->getMessage());
+                        $keyOrPwd = '';
+                    }
+                }
+                else {
+                    $keyOrPwd = $pass;
+                    log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Authentification SSH par Mot de passe');
+                }
+
+                try {
+                    if (!$sshconnection->login($user, $keyOrPwd)) {
+                        log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] Login ERROR :: ' . $user);
+                        $cnx_ssh = 'KO';
+                    }
+                } catch (Exception $e) {
+                    log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] Authentification SSH :: '. $e->getMessage());
+                    $cnx_ssh = 'KO';
+                }
+
+                try {
+                    if ($sshconnection->isConnected()) {
+                        log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH (isConnected) :: OK');
+                        if ($sshconnection->isAuthenticated()) {
+                            log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH (isAuthenticated) :: OK');
+                        }
+                        else {
+                            log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] Connexion SSH (isAuthenticated) :: KO');
+                            log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH LastError :: ' . $sshconnection->getLastError());
+                            $cnx_ssh = 'KO';
+                        }
+                    } else {
+                        log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] Connexion SSH (isConnected) :: KO');
+                        log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH LastError :: ' . $sshconnection->getLastError());
+                        $cnx_ssh = 'KO';
+                    }
+                } catch (Exception $e) {
+                    log::add(__CLASS__, 'error', '['. $equipement .'][connectSSH] Connexion SSH :: '. $e->getMessage());
+                    log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH Log :: ' . $sshconnection->getLog());
+                    $cnx_ssh = 'KO';
+                }
+
+                // Fin de la connexion SSH
+                if ($cnx_ssh != 'KO') {
+                    $cnx_ssh = 'OK';
+                    log::add(__CLASS__, 'debug', '['. $equipement .'][connectSSH] Connexion SSH (cnx_ssh) :: OK');
+                    // Code à exécuter si la connexion SSH est OK
+                }
+            }
+        }
+
     }
 }
 
