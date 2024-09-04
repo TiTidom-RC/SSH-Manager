23e40036932705858ba0d7471cc5d3db44368dc0
Mips2648 <mips2648@gmail.com>
2024-09-04 09:05:01 +0200

    add cron minute

--- core/class/sshmanager.class.php
+++ core/class/sshmanager.class.php
@@ -69,6 +69,24 @@ class sshmanager extends eqLogic {
         return $pluginVersion;
     }
 
+
+    public static function cron() {
+        /** @var sshmanager */
+        foreach (self::byType(__CLASS__, true) as $sshmanager) {
+            $autorefresh = $sshmanager->getConfiguration('autorefresh');
+            if ($autorefresh == '')  continue;
+            try {
+                $cron = new Cron\CronExpression($autorefresh, new Cron\FieldFactory);
+                if ($cron->isDue()) {
+                    // TODO: check if any info command with a ssh command to execute
+                }
+            } catch (Exception $e) {
+                log::add(__CLASS__, 'error', __('Expression cron non valide pour ', __FILE__) . $sshmanager->getName() . ' : ' . $autorefresh);
+            }
+        }
+    }
+
+
     // Methods used by client plugins
 
     public static function getRemoteHosts() {
