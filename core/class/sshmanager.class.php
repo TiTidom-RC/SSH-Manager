9d6e798b6e766a3604ee9160a4b3968d3c8d47b5
Olivier <16240457+TiTidom-RC@users.noreply.github.com>
2024-09-07 13:24:28 +0200

    Update sshmanager.class.php

--- core/class/sshmanager.class.php
+++ core/class/sshmanager.class.php
@@ -365,6 +365,7 @@ class sshmanager extends eqLogic {
             $refresh->setLogicalId('refresh');
             $refresh->setIsVisible(1);
             $refresh->setName(__('Rafraichir', __FILE__));
+            $refresh->setConfiguration('cmdType', 'refresh');
             $refresh->setType('action');
             $refresh->setSubType('other');
             $refresh->setEqLogic_id($this->getId());
